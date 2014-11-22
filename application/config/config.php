631255c9337bacc25a3849e322629d78d403dc9d
newbiettn <newbiettn@gmail.com>
2014-11-22 23:35:30 +0100

    Add Factual service

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ihotfood.com/';
+$config['base_url']	= 'http://localhost:8888/ihotfood/';
 
 /*
 |--------------------------------------------------------------------------
