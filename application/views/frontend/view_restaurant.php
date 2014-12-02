96cbca38b9b48d234750e8b8303de9874fc8a3ca
nmduc <minhduc0905@gmail.com>
2014-12-02 02:23:14 +0100

    fix bug showing review

--- application/views/frontend/view_restaurant.php
+++ application/views/frontend/view_restaurant.php
@@ -190,7 +190,6 @@
 					</div>
 					-->
 					<!-- End Comment Input -->
-
 					<!-- User reviews -->
 					<?php foreach ($reviews as $review) { ?>
 						<div class="row single-comment">
@@ -202,6 +201,7 @@
 								<div class="row">
 									<div class="large-12">
 										<a href=""><?php echo($review->user_info['username']); ?></a>
+										<span style="font-size:12px"><i>Posted on: (<?php echo($review->publish_time); ?>)</i></span>
 									</div>
 									<div class="row">
 										<div class="large-8 columns">
@@ -210,39 +210,40 @@
 										<div class="large-2 columns">
 								        	<div class="row" style="position:relative; right:0px">
 								        		<?php if($review->rating == 1) { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1" checked="checked"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" checked="checked"/>
 								        		<?php } else { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" />
 								        		<?php } ?>
 									        	<?php if($review->rating == 2) { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1" checked="checked"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" checked="checked"/>
 								        		<?php } else { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" />
 								        		<?php } ?>
 								        		<?php if($review->rating == 3) { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1" checked="checked"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" checked="checked"/>
 								        		<?php } else { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" />
 								        		<?php } ?>
 								        		<?php if($review->rating == 4) { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1" checked="checked"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" checked="checked"/>
 								        		<?php } else { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" />
 								        		<?php } ?>
 								        		<?php if($review->rating == 5) { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1" checked="checked"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" checked="checked"/>
 								        		<?php } else { ?>
-									        		<input class="star" type="radio" disabled="disabled" value="1"/>
+									        		<input class="star" type="radio" name="rating-<?php echo($review->id)?>" disabled="disabled" />
 								        		<?php } ?>
 								        	</div>
 										</div>
 									</div>
 									<div class="large-12">
-										<span><?php echo($review->content);?></span>
+										<span><?php echo(strip_tags($review->content));?></span>
 									</div>
 								</div>
 							</div>
 						</div>
+						<hr>
 					<?php } ?>
 					<!-- Comment 1 -->
 					<!-- <div class="row single-comment">
@@ -354,7 +355,6 @@
 		function toogle_review_form() {
 			$("#review-form").toggle();
 		}
-
 	</script>
 </body>
 </html>
